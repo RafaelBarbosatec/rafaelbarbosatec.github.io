7f29447606be4631d1def159dabf17a84ed44342
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2020-07-03 14:46:33 -0300

    ätualiza bonfire

--- bonfire/flutter_service_worker.js
+++ bonfire/flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "926cd8008bef60a4e735ba8d18fbac9c",
 "/": "926cd8008bef60a4e735ba8d18fbac9c",
-"main.dart.js": "34446c7abf1902f2d85aa706cb748999",
+"main.dart.js": "53b34eee3e01656b1fa91021f726950d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
