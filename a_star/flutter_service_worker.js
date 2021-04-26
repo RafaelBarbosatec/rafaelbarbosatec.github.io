f984b7cc62ad957d4b772f83b5b82f37d9de9d31
Rafael Almeida Barbosa <rafaelbarbosatec@gmail.com>
2021-04-26 10:46:02 -0300

    ätualiza ezempo a star

--- a_star/flutter_service_worker.js
+++ a_star/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4b6db237b3514a88107a422469adfb0f",
-"index.html": "7b8a7165d6a4414123b7e46877df03e0",
-"/": "7b8a7165d6a4414123b7e46877df03e0",
-"main.dart.js": "16782a44007082f451cad683c8b80137",
+"index.html": "014b103bfd36ffab3575c0d6fd4fcb8a",
+"/": "014b103bfd36ffab3575c0d6fd4fcb8a",
+"main.dart.js": "95d56196a7ae0c32a0bf2248ec6a1e49",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
