7ff25004e1b7d211c52f7e7f2de5f50d9979bac0
rafaelbarbsoatec <rafaelbarbosatec@gmail.com>
2023-12-18 15:27:37 -0300

    atualiza demo

--- openchat/flutter_service_worker.js
+++ openchat/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "2f02b306b05b6daa3c94141dbbfa3f6b",
-"index.html": "4f2de76e5243abcc19af5b516153e6f8",
-"/": "4f2de76e5243abcc19af5b516153e6f8",
-"main.dart.js": "4a9263b40da3f51f62baeb9a944dfc85",
+"index.html": "e1a2ba60afb79984c29d881363a43756",
+"/": "e1a2ba60afb79984c29d881363a43756",
+"main.dart.js": "a70c937f688bf2d90c346fddcc332331",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
